890a833b1116b06e715f5a77bf3f40c4edddc944
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-10-30 00:22:55 -0400

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { OrderMeanuComponent } from './order-meanu/order-meanu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderMeanuComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order-meanu/order-meanu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-meanu',
+  templateUrl: './order-meanu.component.html',
+  styleUrls: ['./order-meanu.component.css']
+})
+export class OrderMeanuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
